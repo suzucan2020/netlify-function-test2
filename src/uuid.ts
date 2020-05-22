7f90732650e9a51bc4052b6be1668401954e971b
suzucan2020 <mybestfsnk@yahoo.co.jp>
2020-05-22 19:39:02 +0900

    fix uuid.ts

--- src/uuid.ts
+++ src/uuid.ts
@@ -1,5 +1,5 @@
 import { APIGatewayProxyEvent, Context, Callback } from 'aws-lambda';
-import uuidv1 from 'uuid/v1'
+import uuidv1 from 'uuid/v1';
 
 exports.handler = (event: APIGatewayProxyEvent, context: Context, callback: Callback) => {
   callback(null, {
