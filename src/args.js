a10bf07149836b5d178b5ed8cce0d6a82d82af27
suzucan2020 <mybestfsnk@yahoo.co.jp>
2020-05-22 14:46:09 +0900

    fixed typo miss

--- src/args.js
+++ src/args.js
@@ -1,4 +1,4 @@
-export.handler = function(event, context, callback){
+exports.handler = function(event, context, callback){
   console.log(JSON.stringify(event, null, 4))
   callback(null, {
     statusCode: 200,
