a2e4e9b6b39448dc764e606338070daed21d697c
suzucan2020 <mybestfsnk@yahoo.co.jp>
2020-05-22 19:04:44 +0900

    add typescripts

---
+++ src/uuid.ts
@@ -0,0 +1,9 @@
+import { APIGatewayProxyEvent, Context, Callback } from 'aws-lambda';
+import uuidv1 from 'uuid/v1'
+
+exports.handler = (event: APIGatewayProxyEvent, context: Context, callback: Callback) => {
+  callback(null, {
+    statusCode: 200,
+    body: uuidv1(),
+  })
+}
