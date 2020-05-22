6c9b77921ab0f2714cd8f43adb8d739cb4bf5592
suzucan2020 <mybestfsnk@yahoo.co.jp>
2020-05-22 19:43:09 +0900

    delete uuid.ts

--- src/uuid.ts
+++
@@ -1,9 +0,0 @@
-import { APIGatewayProxyEvent, Context, Callback } from 'aws-lambda';
-import uuidv1 from 'uuid/v1';
-
-exports.handler = (event: APIGatewayProxyEvent, context: Context, callback: Callback) => {
-  callback(null, {
-    statusCode: 200,
-    body: uuidv1(),
-  })
-}
